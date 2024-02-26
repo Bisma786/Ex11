11583c7eafce90c996989f6906cdda1b816689c3
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:22:24 +0500

    Initial commit

---
+++ ex11.ts
@@ -0,0 +1,8 @@
+// Q:11 Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+let Name:string[]=["Lubna","Uzma","Saba","Azra"];
+console.log(Name[0]);
+console.log(Name[1]);
+console.log(Name[2]);
+console.log(Name[3]);
